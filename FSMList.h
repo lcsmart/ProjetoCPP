a4ca3db50722d1d3869838f3609a84f6c63d7392
Rafael Valente <rfavalente@gmail.com>
2018-11-06 14:29:22 -0200

    Remoção de linhas desnecessárias

--- FSMList.h
+++ FSMList.h
@@ -4,16 +4,12 @@
 
 class List {
 
-  int numElem;
   Node *head;
   Node *ea;
 
 public:
   List();
   ~List();
-  // int getNumElem();
-  // void insertionSort(Node *func); // Em ordem crescente da matricula
-  //  int removeNode(int sta);        // Por numero de matricula
   int entradaM025();
   int entradaM050();
   int entradaM100();
